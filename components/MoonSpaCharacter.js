68c907f6940d6dc1b5bf0189450e315e6a162670
bunchesbunches66-ship-it <bunchesbunches66@gmail.com>
2026-04-04 22:47:50 -0700

    Add MoonSpaCharacter component

---
+++ components/MoonSpaCharacter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MoonSpaCharacter = () => {
+    return (
+        <div>
+            <h1>Welcome to the Moon Spa!</h1>
+            <p>Your interactive character awaits!</p>
+        </div>
+    );
+};
+
+export default MoonSpaCharacter;
