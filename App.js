3aebc9de6d8eb8110b7b68427530a37ad572fa1c
bunchesbunches66-ship-it <bunchesbunches66@gmail.com>
2026-04-04 22:47:41 -0700

    Create App.js file as main React Native entry point with MoonSpa character component

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import MoonSpa from './MoonSpa';
+
+const App = () => {
+  return <MoonSpa />;
+};
+
+AppRegistry.registerComponent('MoonSparkles', () => App);
+
+export default App;
